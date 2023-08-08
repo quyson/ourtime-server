c6c307b2da512de805c829db152eb65c83c4be2f
unknown <quyson.nguyen.1999@gmail.com>
2023-08-08 13:24:16 -0700

    Modify program file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using ourTime_server
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,20 +8,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .AllowCredentials();
+        });
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<WebRTC>("/webRTC");
+});
 
 app.Run();

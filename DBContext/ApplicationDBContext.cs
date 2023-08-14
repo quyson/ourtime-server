17dc7f71f1647c6ebf6bb7c531fdc7e76d74beab
unknown <quyson.nguyen.1999@gmail.com>
2023-08-14 14:59:46 -0700

    Create DB Context Service file

---
+++ DBContext/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ourTime_server.Models;
+
+namespace ourTime_server.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,14 +7,14 @@ namespace ourTime_server.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Username is required")]
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
-        public string Email { get; set; }
+        public string Email { get; set; } = "";
 
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters")]
-        public string Password { get; set; }
+        public string Password { get; set; } = "";
 
     }
 }

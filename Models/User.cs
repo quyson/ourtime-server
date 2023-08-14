3b6fb54beca9ca2d2ae0f68f6592c0ef119d9132
unknown <quyson.nguyen.1999@gmail.com>
2023-08-14 13:33:10 -0700

    Create user model

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ourTime_server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters")]
+        public string Password { get; set; }
+
+    }
+}

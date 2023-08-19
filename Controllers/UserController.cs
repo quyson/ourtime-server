bb9940946750121af5f4d77c39eb1dfb393e0d8a
unknown <quyson.nguyen.1999@gmail.com>
2023-08-19 14:46:59 -0700

    Connect to local SQL DB

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,50 +1,62 @@
 ﻿using ourTime_server.Models;
 using System.Threading.Tasks;
+using ourTime_server.DBContext;
 
 namespace ourTime_server.Controllers
 {
-    public class UserController : Controllers
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-        [Route("api/[controller]")]
-        [ApiController]
-        public class UserController : ControllerBase
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
         {
-            private readonly ApplicationDbContext _context; // Replace with your DbContext
+            _context = context;
+        }
 
-            public UserController(ApplicationDbContext context)
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
             {
-                _context = context;
+                return BadRequest(ModelState);
             }
 
-            [HttpPost("register")]
-            public async Task<IActionResult> Register([FromBody] RegistrationModel model)
+            return (model);
+            /*var newUser = new User
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
+                Username = model.Username,
+                Email = model.Email,
+                // You should hash and salt the password before storing it
+                Password = HashAndSaltPassword(model.Password),
+            };
 
-                var newUser = new User
-                {
-                    Username = model.Username,
-                    Email = model.Email,
-                    // You should hash and salt the password before storing it
-                    Password = HashAndSaltPassword(model.Password),
-                };
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
 
-                _context.Users.Add(newUser);
-                await _context.SaveChangesAsync();
+            return Ok(new { Message = "Registration successful" });*/
+        }
 
-                return Ok(new { Message = "Registration successful" });
+        [HttpPost("login")]
+        public IActionResult GetUserProfile(string username)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == username);
+            if (user == null)
+            {
+                return NotFound();
             }
 
-            // Hash and salt password using a secure hashing algorithm (e.g., bcrypt)
-            private string HashAndSaltPassword(string password)
-            {
-                // Implement your password hashing logic here
-            }
+            // Return user profile data
+            return Ok(user);
+        }
+
+        // Hash and salt password using a secure hashing algorithm (e.g., bcrypt)
+        private string HashAndSaltPassword(string password)
+        {
+            // Implement your password hashing logic here
         }
     }
 }
 
-   
+
